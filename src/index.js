723b9ee5804bb1dded63ec384754a29b1e41e4ac
artifishional <artifishional@mail.ru>
2018-09-22 14:36:04 +0300

    - new io - updated

--- src/index.js
+++ src/index.js
@@ -23,11 +23,20 @@ function join(obj) {
  * @param {Function} args.buttons[].onclick
  * @returns {Function}
  */
-export default function ({
-        settings: {button: {size = 40} = {}, width = 10, height = 10, windowed = true} = {},
-        buttons,
-        oninit
- } = {}) {
+export default ( {
+                     stream,
+                     settings: {
+                         button: {
+                             size = 40
+                         } = {},
+                         width = 10,
+                         height = 10,
+                         windowed = true
+                     } = {},
+                     buttons,
+                     oninit
+                 } ) =>
+stream( emt => {
 
     let target;
 
@@ -47,7 +56,13 @@ export default function ({
         window.addEventListener('unload', function(event) {
             win.close();
         });
-        target = win.document.body;
+        if(win.document) {
+            target = win.document.body;
+            console.warn("To display the debugger window, allow pop-ups and refresh the page");
+        }
+        else {
+            target = document.createElement("div");
+        }
     }
     else {
         target = document.createElement("div");
@@ -58,29 +73,25 @@ export default function ({
         window.addEventListener('load', () => document.body.appendChild(target));
     }
 
-    return function (emt) {
+    oninit && oninit(emt);
 
-        oninit && oninit(emt);
+    buttons.map( ({name, onclick, size: {x = 1, y = 1 } = {} }) => {
+        const elm = document.createElement("div");
+        elm.innerHTML = name;
+        elm.style.float = "left";
+        elm.style.color = "#0026ff";
+        elm.style.textAlign = "center";
+        elm.style.font = "30px Consolas";
+        elm.style.border = "1px solid #0026ff";
+        elm.style.margin = "1px";
+        elm.style.width = `${x * (size - 2) }px`;
+        elm.style.height = `${y * (size - 2) }px`;
+        elm.style.lineHeight = `${y * size}px`;
+        elm.style.cursor = "pointer";
+        elm.addEventListener("click", () => onclick(emt));
+        return elm;
+    } )
+        .forEach( elm => target.appendChild(elm) )
+    ;
 
-        buttons.map( ({name, onclick, size: {x = 1, y = 1 } = {} }) => {
-            const elm = document.createElement("div");
-            elm.innerHTML = name;
-            elm.style.float = "left";
-            elm.style.color = "#0026ff";
-            elm.style.textAlign = "center";
-            elm.style.font = "30px Consolas";
-            elm.style.border = "1px solid #0026ff";
-            elm.style.margin = "1px";
-            elm.style.width = `${x * (size - 2) }px`;
-            elm.style.height = `${y * (size - 2) }px`;
-            elm.style.lineHeight = `${y * size}px`;
-            elm.style.cursor = "pointer";
-            elm.addEventListener("click", () => onclick(emt));
-            return elm;
-        } )
-            .forEach( elm => target.appendChild(elm) )
-        ;
-
-    }
-
-};
+});
