12285d9d320b9845600d28ce1a844f0b03d51f9c
artifishional <artifishional@mail.ru>
2018-09-22 16:43:22 +0300

    - new io - updated

--- src/index.js
+++ src/index.js
@@ -36,62 +36,62 @@ export default ( {
                      buttons,
                      oninit
                  } ) =>
-stream( emt => {
+    stream( emt => {
 
-    let target;
+        let target;
 
-    if(windowed) {
-        const win = window.open(null, null, join({
-            directories: "no",
-            titlebar: "no",
-            toolbar: "no",
-            location: "no",
-            status: "no",
-            menubar: "no",
-            scrollbars: "no",
-            resizable: "no",
-            width: width * size,
-            height: height * size
-        }));
-        window.addEventListener('unload', function(event) {
-            win.close();
-        });
-        if(win.document) {
-            target = win.document.body;
-            console.warn("To display the debugger window, allow pop-ups and refresh the page");
+        if(windowed) {
+            let win = window.open("", null, join({
+                directories: "no",
+                titlebar: "no",
+                toolbar: "no",
+                location: "no",
+                status: "no",
+                menubar: "no",
+                scrollbars: "no",
+                resizable: "no",
+                width: width * ( size + 4 ),
+                height: height * ( size + 4 )
+            }));
+            if(win && win.document) {
+                win.focus();
+                target = win.document.body;
+            }
+            else {
+                console.warn("To display the debugger window, allow pop-ups and refresh the page");
+                target = document.createElement("div");
+            }
+            target.style.margin = "0px";
+            target.style.padding = "0px";
+            target.style.top = "0px";
+            target.style.left = "0px";
+            target.style.zIndex = 999999999;
         }
         else {
-            target = document.createElement("div");
+            throw "unwindowed mode now is unsupported"
         }
-    }
-    else {
-        target = document.createElement("div");
-        target.style.position = "absolute";
-        target.style.top = "0px";
-        target.style.left = "0px";
-        target.style.zIndex = 999999999;
-        window.addEventListener('load', () => document.body.appendChild(target));
-    }
 
-    oninit && oninit(emt);
+        target.innerHTML = "";
 
-    buttons.map( ({name, onclick, size: {x = 1, y = 1 } = {} }) => {
-        const elm = document.createElement("div");
-        elm.innerHTML = name;
-        elm.style.float = "left";
-        elm.style.color = "#0026ff";
-        elm.style.textAlign = "center";
-        elm.style.font = "30px Consolas";
-        elm.style.border = "1px solid #0026ff";
-        elm.style.margin = "1px";
-        elm.style.width = `${x * (size - 2) }px`;
-        elm.style.height = `${y * (size - 2) }px`;
-        elm.style.lineHeight = `${y * size}px`;
-        elm.style.cursor = "pointer";
-        elm.addEventListener("click", () => onclick(emt));
-        return elm;
-    } )
-        .forEach( elm => target.appendChild(elm) )
-    ;
+        oninit && oninit(emt);
 
-});
+        buttons.map( ({name, onclick, size: {x = 1, y = 1 } = {} }) => {
+            const elm = document.createElement("div");
+            elm.innerHTML = name;
+            elm.style.float = "left";
+            elm.style.color = "#0026ff";
+            elm.style.textAlign = "center";
+            elm.style.font = "30px Consolas";
+            elm.style.border = "1px solid #0026ff";
+            elm.style.margin = "1px";
+            elm.style.width = `${x * size + (x-1)*4 }px`;
+            elm.style.height = `${y * size + (y-1)*4 }px`;
+            elm.style.lineHeight = `${y * size}px`;
+            elm.style.cursor = "pointer";
+            elm.addEventListener("click", () => onclick(emt));
+            return elm;
+        } )
+            .forEach( elm => target.appendChild(elm) )
+        ;
+
+    });
