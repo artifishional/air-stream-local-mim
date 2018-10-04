86a7be25288a6d0bfb7bce2930b7f2fbefba4b27
aa_trusov <aa_trusov@bingo-boom.ru>
2018-10-04 18:41:20 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -36,62 +36,71 @@ export default ( {
                      buttons,
                      oninit
                  } ) =>
-stream( emt => {
-
-    let target;
-
-    if(windowed) {
-        let win = window.open("", null, join({
-            directories: "no",
-            titlebar: "no",
-            toolbar: "no",
-            location: "no",
-            status: "no",
-            menubar: "no",
-            scrollbars: "no",
-            resizable: "no",
-            width: width * ( size + 4 ),
-            height: height * ( size + 4 )
-        }));
-        if(win && win.document) {
-            win.focus();
-            target = win.document.body;
+    stream( (emt, {hook}) => {
+        let target;
+        if(windowed) {
+            let win = window.open("", null, join({
+                directories: "no",
+                titlebar: "no",
+                toolbar: "no",
+                location: "no",
+                status: "no",
+                menubar: "no",
+                scrollbars: "no",
+                resizable: "no",
+                width: width * ( size + 4 ),
+                height: height * ( size + 4 )
+            }));
+            if(win && win.document) {
+                win.focus();
+                target = win.document.body;
+            }
+            else {
+                console.warn("To display the debugger window, allow pop-ups and refresh the page");
+                target = document.createElement("div");
+            }
+            target.style.margin = "0px";
+            target.style.padding = "0px";
+            target.style.top = "0px";
+            target.style.left = "0px";
+            target.style.zIndex = 999999999;
         }
         else {
-            console.warn("To display the debugger window, allow pop-ups and refresh the page");
-            target = document.createElement("div");
+            throw "unwindowed mode now is unsupported"
         }
-        target.style.margin = "0px";
-        target.style.padding = "0px";
-        target.style.top = "0px";
-        target.style.left = "0px";
-        target.style.zIndex = 999999999;
-    }
-    else {
-        throw "unwindowed mode now is unsupported"
-    }
 
-    target.innerHTML = "";
 
-    oninit && oninit(emt);
+        oninit && oninit(emt);
 
-    buttons.map( ({name, onclick, size: {x = 1, y = 1 } = {} }) => {
-        const elm = document.createElement("div");
-        elm.innerHTML = name;
-        elm.style.float = "left";
-        elm.style.color = "#0026ff";
-        elm.style.textAlign = "center";
-        elm.style.font = "30px Consolas";
-        elm.style.border = "1px solid #0026ff";
-        elm.style.margin = "1px";
-        elm.style.width = `${x * size + (x-1)*4 }px`;
-        elm.style.height = `${y * size + (y-1)*4 }px`;
-        elm.style.lineHeight = `${y * size}px`;
-        elm.style.cursor = "pointer";
-        elm.addEventListener("click", () => onclick(emt));
-        return elm;
-    } )
-        .forEach( elm => target.appendChild(elm) )
-    ;
+        hook.add( ({status}) => {
+            target.innerHTML = "";
+            status && buttons.map( ({name, onclick, size: {x = 1, y = 1 } = {} }) => {
+                const elm = document.createElement("div");
+                elm.innerHTML = name;
+                elm.style.float = "left";
+                elm.style.color = "#0026ff";
+                elm.style.textAlign = "center";
+                elm.style.font = "30px Consolas";
+                elm.style.border = "1px solid #0026ff";
+                elm.style.margin = "1px";
+                elm.style.width = `${x * size + (x-1)*4 }px`;
+                elm.style.height = `${y * size + (y-1)*4 }px`;
+                elm.style.lineHeight = `${y * size}px`;
+                elm.style.cursor = "pointer";
+                const isNotActive = (status === "pending" && name !== "end bets") ||
+                    (status === "playing" && typeof(name) !== "number") ||
+                    (typeof(status) === "number" && name !== "end game") || (status === "finished")
+                if(isNotActive){
+                    elm.style.backgroundColor = "#999999"
+                    elm.style.pointerEvents = "none"
+                }
+                elm.addEventListener("click", () => onclick(emt));
+                return elm;
+            } )
+                .forEach( elm => {
+                    target.appendChild(elm)
+                })
+            ;
+        } );
 
-});
+    });
