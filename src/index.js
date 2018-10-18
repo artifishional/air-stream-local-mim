15c8cbf0541392e6f4b07aa93d08a9acbab253ee
Полицинский Артем Сергеевич <artifishional@mail.ru>
2018-10-18 15:53:01 +0300

    - io - updated

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ export default ( {
 
         oninit && oninit(emt);
 
-        hook.add( ({ action, sections,names }) => {
+        hook.add( ({ action, sections = {}, names = {} }) => {
             if( action === "set-state-active" ) {
                 target.innerHTML = "";
                 buttons.map(({section, name, onclick, size: {x = 1, y = 1} = {}}, i) => {
